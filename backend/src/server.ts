bef55db454feb22b56b2344b08b3153eebbfe549
Adeyeye Ayodeji Daniel <adeyeye17@gmail.com>
2024-11-22 04:45:55 +0100

    Added Vercel configuration

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -161,4 +161,5 @@ const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-module.exports = app;
+
+module.exports = (req: Request, res: Response) => app(req, res);
